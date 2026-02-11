2c8af29cce67b2c78adaddb74311cc187ec51170
Anthony Phillips <a.phillips@comsoc.org>
2026-02-11 17:00:02 -0500

    \Fix canvas module, config sync, admin theme styling, add login redirect to /admin/content"

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'c1566bf95e636fc1';
+  public const string VERSIONS_HASH = 'bab9806818b44e72';
 
 }
